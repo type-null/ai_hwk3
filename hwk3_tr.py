3618841226116d3a9e225f465f35629d5388bc60
Weihang Ren <rwh18@outlook.com>
2020-02-13 12:13:03 -0500

    add one for all func

--- hwk3_tr.py
+++ hwk3_tr.py
@@ -9,15 +9,25 @@ def clean_text(tweet) -> str:
 
 def tweetTokenizer(text) -> list:
     """Load nltk.tokenize.TweetTokenizer"""
+    return
 
 def tokenize_text(cleaned_text) -> list:
     """Also filter out stop words"""
     token = cleaned_text.split()
     return [i for i in token if i not in stopwords.words()]
 
-def replace_token_with_index() -> list:
-    
-    return 
+def replace_token_with_index(tokenized_text) -> list:
+    indexOfTweet = []
+    with open("index_dict.txt") as indexDict:
+        for token in tokenized_text:
+            indexOfTweet.append(1)
+    return indexOfTweet
 
-def pad_sequence() -> list:
-    return 
+def pad_sequence(index_list) -> list:
+    return "Yes"
+
+def oneForAll(tweet) -> list:
+    """Do all conversion at once"""
+    return pad_sequence(replace_token_with_index(
+        tokenize_text(clean_text(tweet))))
+
